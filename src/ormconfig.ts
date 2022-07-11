def9613f51a195a7921498579600c49092abdab7
Jason <jasonkelly6@gmail.com>
2022-07-10 22:19:35 -0400

    add migrations

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,6 +1,6 @@
-import { DataSourceOptions } from 'typeorm';
+import { ConnectionOptions } from 'typeorm';
 
-const config: DataSourceOptions = {
+const config: ConnectionOptions = {
   type: 'postgres',
   host: process.env.HOST,
   port: Number(process.env.DBPORT),
@@ -8,7 +8,11 @@ const config: DataSourceOptions = {
   password: process.env.PASSWORD,
   database: process.env.DB,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 };
 
 export default config;
